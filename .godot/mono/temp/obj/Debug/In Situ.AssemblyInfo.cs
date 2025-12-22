3a7e8be867ecde66c6ba3bb0b851f47d5d35d02b
ShichiNoBushi <shichinobushi@gmail.com>
2025-12-21 20:04:26 -0600

    Added laminate substrate recipe and adjusted early recipes.

--- .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c095c0b41c78137de5252e98da549ba887b79678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c85ef0d4efc575398c9201e4b2e480674672a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
