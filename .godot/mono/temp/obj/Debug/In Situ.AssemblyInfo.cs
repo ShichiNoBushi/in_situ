afbd4e55f8fc581d7a5ec260c939b3241ebb5577
ShichiNoBushi <shichinobushi@gmail.com>
2025-12-25 17:32:59 -0600

    Added recipe to reduce sand to silicon.

--- .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e88f1183e438d9e1c5272d0970a554edf8e37a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f6b8a9f19e68ff0b909c82f6a48f02adbfa0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- .godot/mono/temp/obj/ExportDebug/osx-x64/In Situ.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportDebug/osx-x64/In Situ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc328967bf2f5dc6941fe4a6e4e9a3f0209e2d91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f6b8a9f19e68ff0b909c82f6a48f02adbfa0c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
