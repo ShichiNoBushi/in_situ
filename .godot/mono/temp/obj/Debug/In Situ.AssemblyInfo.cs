1e960fe29bb3bb949d4ac3485cf9c02ca5d500b7
ShichiNoBushi <shichinobushi@gmail.com>
2026-04-07 21:06:16 -0500

    Corrected minor resource, recipe, and quest data. Issue with selecting Radio Transceiver recipe.

--- .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/In Situ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e967f0b006d978d9474e39757c3969cb7730a7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294d2efd4252222944b33d22f8b386733e5c6449")]
 [assembly: System.Reflection.AssemblyProductAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("In Situ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
